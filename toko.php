501799b7059204dab38de1adf2ced908f9f6ce89
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-06-11 10:40:50 +0700

    xx

--- toko.php
+++ toko.php
@@ -1,17 +1,114 @@
-
-
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Toko Elektronik</title>
+
+    <style>
+        .nav {
+            background: blue;
+        }
+    </style>
 </head>
+
 <body>
-    <ul>
-        <li>Tes</li>
-    </ul>
+
+    <!doctype html>
+    <html lang="en">
+
+    <head>
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+            integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+        <title>Hello, world!</title>
+    </head>
+
+    <body>
+        <div class="container my-4">
+            <h1>Toko Elektronik</h1>
+            <h3>Daftar harga barang elektronik</h3>
+            <br>
+            <br>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">No.</th>
+                        <th scope="col">Kode</th>
+                        <th scope="col">Nama Barang</th>
+                        <th scope="col">Harga</th>
+                        <th scope="col">Diskon</th>
+                        <th scope="col">Stok</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>1B001</td>
+                        <td>Monitor Samsung 15'</td>
+                        <td>Rp.500.000</td>
+                        <td>7%</td>
+                        <td>5</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>2B002</td>
+                        <td>Monitor LG 14'</td>
+                        <td>Rp.400.000</td>
+                        <td>2%</td>
+                        <td>3</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>3B003</td>
+                        <td>CD-RW Samsung</td>
+                        <td>Rp.300.000</td>
+                        <td>10%</td>
+                        <td>10</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>4B004</td>
+                        <td>Memory Card 256</td>
+                        <td>Rp.450.000</td>
+                        <td>0%</td>
+                        <td>6</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>5B005</td>
+                        <td>Hardisk Seagate 512GB</td>
+                        <td>Rp.570.000</td>
+                        <td>5%</td>
+                        <td>5</td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+
+            <!-- Optional JavaScript; choose one of the two! -->
+
+            <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+            <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+                integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+                crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF"
+                crossorigin="anonymous"></script>
+
+            <!-- Option 2: Separate Popper and Bootstrap JS -->
+            <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    -->
+    </body>
+
+    </html>
+
+
 </body>
+
 </html>
