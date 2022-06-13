b71fd10a1fd82794bfc96a6c00e6bd34d70aae1f
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-06-13 09:39:26 +0700

    .

---
+++ entri.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Data Entri</title>
+</head>
+<body>
+    <?php
+        $dsn = "mysql:host=localhost;dbname=bukuDb";
+        $namauser = "admin";
+        $password = "12345";
+
+        function tampilData($conn){
+            $SQL = "SELECT * FROM PEN"
+        }
+    ?>
+</body>
+</html>
