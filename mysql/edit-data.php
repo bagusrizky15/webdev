f70c9acbf4be495c8be6d460e9452a59ce4b9478
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-06-26 18:54:23 +0700

    edit

--- mysql/edit-data.php
+++ mysql/edit-data.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -8,47 +9,42 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
-    <title>Data Mhs</title>
-  </head>
-  <body>
+    <title>Create</title>
+</head>
+
+<body>
     <div class="container">
-    <a href="create.php" class="btn btn-success">Create Data</a>
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nama</th>
-      <th scope="col">NIM</th>
-      <th scope="col">Kelas</th>
-      <th scope="col">Gender</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  
-  <tbody>
-  <?php
-    include 'koneksi.php';
-    $no = 1;
-    $mahasiswa = mysqli_query($koneksi, 'SELECT * FROM mahasiswa');
-    while ($data = mysqli_fetch_array($mahasiswa)) {
-        # code...
-  ?>
-    <tr>
-      <th scope="row"><?= $no; ?></th>
-      <td><?= $data['name']; ?></td>
-      <td><?= $data['nim']; ?></td>
-      <td><?= $data['kelas']; ?></td>
-      <td><?= $data['gender']; ?></td>
-      <td>
-        <a href="edit-data.php?id=<?php $data['id']?>" class="btn btn-warning">Edit</a>
-      </td>
-    </tr>
-    <?php
-    $no++;
-    }
-?>
-  </tbody>
-</table>
+        <h1>Index Data Mahasiswa</h1>
+        <form action="terima-data.php" method="POST" enctype="multipart/form-data">
+            <?php
+                include 'koneksi.php';
+                $id = $_GET['id'];
+                $query = mysqli_query($koneksi, "SELECT * FROM mahasiswa WHERE id='$id'");
+                while ($data = mysqli_fetch_array($query)) 
+                {
+            ?>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Nama</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="name" value="<?= $data['name']?>">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">NIM</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="nim" value="<?= $data['nim']?>">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Kelas</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="kelas" value="<?= $data['kelas']?>">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Gender</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1"  name="gender" value="<?= $data['gender']?>">
+            </div>
+            <?php
+                }
+            ?>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
     </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
@@ -63,5 +59,6 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
